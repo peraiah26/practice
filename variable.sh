3a4655d765cb325e4fba8724c6228caec7b6ae9b
name <peraiahtellagorla92@gmail.com>
2026-05-23 23:22:50 +0530

    sc

---
+++ variable.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "peraiah":: hii veera how are you.
+echo "veera"::i am good peraiah what abou you.
+echo "peraiah":: i am aslo good veera what about ur job.
+echo "veera":: good peraiah thank you.
