096ff5aaf70c9e511ac5f5b8c6300c733684f340
name <peraiahtellagorla92@gmail.com>
2026-05-25 16:23:00 +0530

    tc

---
+++ 01-variable.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+person1="$1"
+person2="$2"
+echo "$person1:: hii $person2 how are you"
+echo "$person2::i am good $person1 what abou you"
+echo "$person1:: i am aslo good $person2 what about ur job"
+echo "$person2:: good $person1 thank you"
