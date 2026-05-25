22c8a938f957d972c8857fcdec8e3b112d4736e8
name <peraiahtellagorla92@gmail.com>
2026-05-25 16:53:24 +0530

    tc

--- spl-var.sh
+++ spl-var.sh
@@ -12,3 +12,8 @@ total_time=$(($end_time - $start_time))
 echo "script executed time is:${total_time}"
 
 echo "current dir: $PWD"
+echo "who runing this: $USER"
+echo "home dir" : $HOME"
+echo "script name is: $0"
+echo "process id is : $PID"
+
